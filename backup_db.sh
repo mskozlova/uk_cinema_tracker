625e935e134c0a614f037fa175f05202cf405ff0
mskozlova <be-more-lavender@yandex.ru>
2023-03-25 20:58:40 +0000

    better backup script

--- backup_db.sh
+++ backup_db.sh
@@ -1,3 +1,13 @@
 set -eux
+
+original_filename=movies.db
 backup_filename=db_backup/movies_$(date +"%Y-%m-%dT%T.%N" -u).db.zstd
-find db_backup -mtime +30 -delete && zstd --ultra -22 movies.db -o $backup_filename 
+
+if [ ! -f "$original_filename" ]; then
+    echo "$original_filename does not exist."
+    exit 1
+fi
+
+mkdir db_backup -p
+find db_backup -mtime +30 -delete
+zstd --ultra -22 $original_filename -o $backup_filename 
