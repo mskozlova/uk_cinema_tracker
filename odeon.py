72b0bac6ceee30bd1ad8e66c812b7c39a1ece465
mskozlova <be-more-lavender@yandex.ru>
2023-04-27 20:21:59 +0000

    parse movie images from odeon

--- odeon.py
+++ odeon.py
@@ -1,9 +1,10 @@
-import requests
 import structs
 import logging
 import datetime
-import time
 import re
+import httplib
+import subprocess
+import time
 
 from typing import List
 
@@ -15,7 +16,7 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
 
-def get_all_venues(**kwargs) -> List[structs.Venue]:
+def get_all_venues(revision, **kwargs) -> List[structs.Venue]:
     url = 'https://odeon-vwc.webtrends-optimize.workers.dev/CinemasSchedule'
     headers = {
   'authority': 'odeon-vwc.webtrends-optimize.workers.dev',
@@ -31,9 +32,7 @@ def get_all_venues(**kwargs) -> List[structs.Venue]:
   'sec-fetch-site': 'cross-site',
   'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36',
     }
-    r = requests.get(url, headers=headers)
-    time.sleep(0.1)
-    js = r.json()
+    js = httplib.get_json(revision, url, headers)
     venues = []
     for item in js:
         id_ = item['ID']
@@ -45,6 +44,35 @@ def get_all_venues(**kwargs) -> List[structs.Venue]:
         link = 'https://www.odeon.co.uk/cinemas/' + name.lower().replace(' ', '-') + '/'
         venues.append(structs.Venue(id_, normalized_name, 'ODEON', lat, lon, link, True))
     return venues
+
+def get_movie_image(revision: int, link: str):
+    args = [
+        'curl', '-s', '-L', link,
+        '-H', "authority: www.odeon.co.uk",
+        '-H', "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        '-H', "accept-language:ru,en;q=0.9",
+        '-H', "cache-control:max-age=0",
+        '-H', "sec-ch-ua:\"Not.A/Brand\";v=\"8\",\"Chromium\";v=\"114\",\"YaBrowser\";v=\"23\"",
+        '-H', "sec-ch-ua-mobile:?1",
+        '-H', "sec-ch-ua-platform:Android",
+        '-H', "sec-fetch-dest:document",
+        '-H', "sec-fetch-mode:navigate",
+        '-H', "sec-fetch-site:none",
+        '-H', "sec-fetch-user:?1",
+        '-H', "upgrade-insecure-requests:1",
+        '-H', "user-agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile \"Safari/537.36\""]
+    o = subprocess.check_output(args)
+    time.sleep(0.1)
+    text = o.decode()
+    for line in text.split('\n'):
+        line = line.strip()
+        if '<img' not in line:
+            continue
+        if 'class="v-image__img"' not in line:
+            continue
+        return line.split('src="', 1)[1].split('">')[0]
+    logger.error(f"Cannot find image in {link}")
+    return None
 
 
 def get_all_movies(revision: int, **kwargs) -> List[structs.Movie]:
@@ -63,8 +91,7 @@ def get_all_movies(revision: int, **kwargs) -> List[structs.Movie]:
   'sec-fetch-site': 'cross-site',
   'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36',
     }
-    r = requests.get(url, headers=headers)
-    js = r.json()
+    js = httplib.get_json(revision, url, headers)
     movies = []
     for item in js:
         id_ = item['ID']
@@ -75,12 +102,12 @@ def get_all_movies(revision: int, **kwargs) -> List[structs.Movie]:
         running_time = item['RunTime']
         release_date = item['OpeningDate'] # ?
         trailer_link = item['TrailerUrl']
-        poster_link = item['GraphicUrl']
         normalized_name = re.sub('[^a-z0-9\-]', '', title.lower().replace(' ', '-'))
         link = f'https://www.odeon.co.uk/films/{normalized_name}/{id_}'
+        image_link = get_movie_image(revision, link)
         additional_info = {
             'synopsis': synopsis,
-            'image_link': poster_link,
+            'image_link': image_link,
             'trailer_link': trailer_link,
         }
         movies.append(structs.Movie(id_, title, 'ODEON', link, True, additional_info))
@@ -88,7 +115,7 @@ def get_all_movies(revision: int, **kwargs) -> List[structs.Movie]:
     return movies
 
 
-def get_all_showings(**kwargs) -> List[structs.Showing]:
+def get_all_showings(revision: int, **kwargs) -> List[structs.Showing]:
     url = 'https://odeon-vwc.webtrends-optimize.workers.dev/FilmsSchedule'
     headers = {
   'authority': 'odeon-vwc.webtrends-optimize.workers.dev',
@@ -104,9 +131,7 @@ def get_all_showings(**kwargs) -> List[structs.Showing]:
   'sec-fetch-site': 'cross-site',
   'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36',
     }
-    r = requests.get(url, headers=headers)
-    time.sleep(0.1)
-    js = r.json()
+    js = httplib.get_json(revision, url, headers)
     all_showings = []
     for item in js:
         title = item['Title']
