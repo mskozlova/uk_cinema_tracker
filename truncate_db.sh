91396aaa9e76f6e8ab1d54460f113f96f0e68f8d
mskozlova <be-more-lavender@yandex.ru>
2023-06-28 22:49:42 +0000

    fix truncate script

--- truncate_db.sh
+++ truncate_db.sh
@@ -1,4 +1,4 @@
-timestamp_week_ago="$(date +'%s')000000"
+timestamp_week_ago="$(date +'%s' --date '7 days ago')000000"
 
 sqlite3 movies.db "delete from venues where revision < $timestamp_week_ago"
 sqlite3 movies.db "delete from movies where revision < $timestamp_week_ago"
